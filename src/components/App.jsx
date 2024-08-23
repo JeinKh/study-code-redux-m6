c324ede311370151ea452c8ed0637fd339dab869
JeinKh <juliakhezhun@gmail.com>
2024-08-23 11:17:56 +0300

    use selector

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
+import Counter from "./Counter/Counter";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,5 +1,26 @@
+import { useSelector } from "react-redux";
+
 const Counter = () => {
-  return <div>Counter</div>;
+  const counter = useSelector((state) => state.counter.counter);
+  const step = useSelector((state) => state.counter.step);
+
+  const handlePlusClick = () => {};
+  const handleMinusClick = () => {};
+  const handleResetClick = () => {};
+  const handleChangeStep = (e) => {};
+  return (
+    <div>
+      <div>
+        <h1>{counter}</h1>
+        <input value={step} onChange={handleChangeStep} />
+        <div>
+          <button onClick={handleMinusClick}>Minus</button>
+          <button onClick={handleResetClick}>Reset</button>
+          <button onClick={handlePlusClick}>Plus</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
